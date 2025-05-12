96039ff247b33c847263ebb31e92678d7fa082da
fahadmaqsood <qazifahadmaqsood@gmail.com>
2025-05-12 21:30:35 +0500

    edited links

--- pages/_document.js
+++ pages/_document.js
@@ -12,12 +12,12 @@ class MyDocument extends Document {
         <Head />
         <body>
           <div className="container">
-            <h1 id="logo" className="text-center"><a href="/">Asad<b>Memon</b></a></h1>
+            <h1 id="logo" className="text-center"><a href="/">Fahad Maqsood <b>Qazi</b></a></h1>
             <ul className="menu">
               <li><a href="/projects">All Projects</a></li>
-              <li><a href="/talks">Talks</a></li>
-              <li><a href="/blog">Blog</a></li>
-              <li><a target="_blank" href="https://asad.substack.com/">Newsletter</a></li>
+              {/* <li><a href="/talks">Talks</a></li> */}
+              {/* <li><a href="/blog">Blog</a></li> */}
+              {/* <li><a target="_blank" href="https://asad.substack.com/">Newsletter</a></li> */}
               <li><a target="_blank" href="https://github.com/fahadmaqsood">Github</a></li>
               <li><a target="_blank" href="https://www.linkedin.com/in/fahad-maqsood-qazi">LinkedIn</a></li>
               <li><a target="_blank" href="https://www.instagram.com/ifahadmaqsood">Instagram</a></li>
@@ -27,7 +27,7 @@ class MyDocument extends Document {
             <NextScript />
 
             <div className="footer">
-              <p>AsadMemon.com [<a href="https://github.com/asadm/asadm.github.io" target="_blank">source code</a>].</p>
+              <p>Fahad Maqsood Qazi [<a href="https://github.com/fahadmaqsood/fahadmaqsood.github.io" target="_blank">source code</a>].</p>
             </div>
           </div>
         </body>
