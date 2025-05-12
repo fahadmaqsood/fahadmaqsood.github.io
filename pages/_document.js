cbf64be9fd45bc57a310284e52c4daa6cce7244c
fahadmaqsood <qazifahadmaqsood@gmail.com>
2025-05-12 21:08:04 +0500

    edited links

--- pages/_document.js
+++ pages/_document.js
@@ -11,25 +11,25 @@ class MyDocument extends Document {
       <Html>
         <Head />
         <body>
-        <div className="container">
-          <h1 id="logo" className="text-center"><a href="/">Asad<b>Memon</b></a></h1>
-          <ul className="menu">
-            <li><a href="/projects">All Projects</a></li>
-            <li><a href="/talks">Talks</a></li>
-            <li><a href="/blog">Blog</a></li>
-            <li><a target="_blank" href="https://asad.substack.com/">Newsletter</a></li>
-            <li><a target="_blank" href="https://github.com/asadm">Github</a></li>
-            <li><a target="_blank" href="https://www.linkedin.com/in/memonasad/">LinkedIn</a></li>
-            <li><a target="_blank" href="http://instagram.com/snapmemon">Instagram</a></li>
-          </ul>
+          <div className="container">
+            <h1 id="logo" className="text-center"><a href="/">Asad<b>Memon</b></a></h1>
+            <ul className="menu">
+              <li><a href="/projects">All Projects</a></li>
+              <li><a href="/talks">Talks</a></li>
+              <li><a href="/blog">Blog</a></li>
+              <li><a target="_blank" href="https://asad.substack.com/">Newsletter</a></li>
+              <li><a target="_blank" href="https://github.com/fahadmaqsood">Github</a></li>
+              <li><a target="_blank" href="https://www.linkedin.com/in/fahad-maqsood-qazi">LinkedIn</a></li>
+              <li><a target="_blank" href="https://www.instagram.com/ifahadmaqsood">Instagram</a></li>
+            </ul>
 
-          <Main />
-          <NextScript />
+            <Main />
+            <NextScript />
 
-          <div className="footer">
-            <p>AsadMemon.com [<a href="https://github.com/asadm/asadm.github.io" target="_blank">source code</a>].</p>
+            <div className="footer">
+              <p>AsadMemon.com [<a href="https://github.com/asadm/asadm.github.io" target="_blank">source code</a>].</p>
+            </div>
           </div>
-        </div>
         </body>
       </Html>
     )
